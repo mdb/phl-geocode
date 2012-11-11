a9de84d3186044bc691c45f5814aa2cecc493837
Mike Ball <mikedball@gmail.com>
2012-11-11 09:41:01 -0500

    Added fixtures directory

---
+++ test/fixtures/locations.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    Address: {
+      StandardizedAddress: 'first address',
+      Similarity: 100
+    },
+    YCoord: 'first y coordinate',
+    XCoord: 'first x coordinate'
+  },
+  {
+    Address: {
+      StandardizedAddress: 'second address',
+      Similarity: 90 
+    },
+    YCoord: 'second y coordinate',
+    XCoord: 'second x coordinate'
+  }
+];
---
+++ test/fixtures/response.js
@@ -0,0 +1,59 @@
+module.exports = {
+  "OriginalAddress":"1500 market street",
+  "Locations": [
+    {
+      "Address": {
+        "StandardizedAddress":"1500 MARKET ST",
+        "AliasMatch":null,
+        "Similarity":100,
+        "StreetNumber":1500,
+        "StreetNumberSuffix":null,
+        "Unit":"",
+        "StreetCode":53560,
+        "StreetFullName":"MARKET ST",
+        "StreetPrefix":null,
+        "StreetName":"MARKET",
+        "StreetType":"ST",
+        "StreetSuffix":null,
+        "StreetCode2":0,
+        "StreetFullName2":null,
+        "StreetPrefix2":null,
+        "StreetName2":null,
+        "StreetType2":null,
+        "StreetSuffix2":null
+      },
+      "XCoord":-75.1661518986459,
+      "YCoord":39.9521740263203,
+      "MatchType":1,
+      "SegmentID":421333,
+      "SegmentID2":-1
+    }, 
+    {
+      "Address": {
+        "StandardizedAddress":"1500S MARKET ST",
+        "AliasMatch":null,
+        "Similarity":99,
+        "StreetNumber":1500,
+        "StreetNumberSuffix":"S",
+        "Unit":"",
+        "StreetCode":53560,
+        "StreetFullName":"MARKET ST",
+        "StreetPrefix":null,
+        "StreetName":"MARKET",
+        "StreetType":"ST",
+        "StreetSuffix":null,
+        "StreetCode2":0,
+        "StreetFullName2":null,
+        "StreetPrefix2":null,
+        "StreetName2":null,
+        "StreetType2":null,
+        "StreetSuffix2":null
+      },
+     "XCoord":-75.1661518986459,
+     "YCoord":39.9521740263203,
+     "MatchType":2,
+     "SegmentID":421333,
+     "SegmentID2":-1
+    }
+  ]
+};
